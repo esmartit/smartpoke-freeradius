2e3356f60bc0fab61bb9f6e4952d4bb471d4dbdc
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-06-29 12:51:11 +0200

    fix: meraki limitation values

--- src/main/kotlin/com/esmartit/freeradiusservice/service/limitations/LimitationService.kt
+++ src/main/kotlin/com/esmartit/freeradiusservice/service/limitations/LimitationService.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 const val ACCT_INTERIM_INTERVAL = "Acct-Interim-Interval"
-const val MAX_UPLOAD = "WISPr-Bandwidth-Max-Up"
-const val MAX_DOWNLOAD = "WISPr-Bandwidth-Max-Down"
+const val MAX_UPLOAD = "Maximum-Data-Rate-Upstream"
+const val MAX_DOWNLOAD = "Maximum-Data-Rate-Downstream"
 const val REDIRECT_URL = "WISPr-Redirection-URL"
 const val MAX_TRAFFIC = "Max-Daily-Octets"
 const val ACCESS_PERIOD = "Access-Period"
