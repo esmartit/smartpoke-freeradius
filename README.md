cb052e5d6afaebfb881b17c203a3b01485d786d2
AZignago <adolfo.zignago@esmartit.es>
2023-01-24 16:56:47 +0100

    fix: comments some lines freeradius_schema

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ build
 
 helm upgrade --install freeradius -f smartpoke-freeradius/values.yaml esmartit-freeradius/smartpoke-freeradius --version 2.4.2
 
-
-
 helm upgrade --install freeradius -f smartpoke-freeradius/values.yaml esmartit-freeradius/smartpoke-freeradius --version 2.4.2
--- src/main/resources/db/migration/V1.0__freeradius_schema.sql
+++ src/main/resources/db/migration/V1.0__freeradius_schema.sql
@@ -202,32 +202,32 @@ create index nas_nasname on nas (nasname);
 /*
  *  Create default administrator for RADIUS
  */
-CREATE USER radius WITH PASSWORD 'radpass';
+-- CREATE USER radius WITH PASSWORD 'radpass';
 
 /*
  * The server can read any table in SQL
  */
-GRANT SELECT ON radcheck TO radius;
-GRANT SELECT ON radreply TO radius;
-GRANT SELECT ON radgroupcheck TO radius;
-GRANT SELECT ON radgroupreply TO radius;
-GRANT SELECT ON radusergroup TO radius;
-GRANT SELECT ON nas TO radius;
+-- GRANT SELECT ON radcheck TO radius;
+-- GRANT SELECT ON radreply TO radius;
+-- GRANT SELECT ON radgroupcheck TO radius;
+-- GRANT SELECT ON radgroupreply TO radius;
+-- GRANT SELECT ON radusergroup TO radius;
+-- GRANT SELECT ON nas TO radius;
 
 /*
  * The server can write to the accounting and post-auth logging table.
  */
-GRANT SELECT, INSERT, UPDATE on radacct TO radius;
-GRANT SELECT, INSERT, UPDATE on radpostauth TO radius;
+-- GRANT SELECT, INSERT, UPDATE on radacct TO radius;
+-- GRANT SELECT, INSERT, UPDATE on radpostauth TO radius;
 
 /*
  * Grant permissions on sequences
  */
-GRANT USAGE, SELECT ON SEQUENCE nas_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radacct_radacctid_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radcheck_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radgroupcheck_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radgroupreply_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radpostauth_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radreply_id_seq TO radius;
-GRANT USAGE, SELECT ON SEQUENCE radusergroup_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE nas_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radacct_radacctid_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radcheck_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radgroupcheck_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radgroupreply_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radpostauth_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radreply_id_seq TO radius;
+-- GRANT USAGE, SELECT ON SEQUENCE radusergroup_id_seq TO radius;
